9daced68ef344e62c0c362bb64eed6696b0cae3d
Atakan Başaran <basaranatakan10@gmail.com>
2024-04-17 00:39:22 +0300

    Initial Commit

---
+++ TicTacToeNew/TicTacToeNewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeNewApp.swift
+//  TicTacToeNew
+//
+//  Created by Atakan Başaran on 17.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeNewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
